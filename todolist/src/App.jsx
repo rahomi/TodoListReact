597ab7ab433b6f4d667e458079beb4e66d5e4887
rahomim <mohib.hossain.bu@gmail.com>
2023-09-09 19:35:48 +0600

    Basic UI design

---
+++ todolist/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import "./styles.css"
+
+export default function App(){
+  return( 
+  <React.Fragment>
+    <form className="new-item-form">
+      <div className="form-row">
+            <label htmlFor="item">New Item</label>
+            <input type="text" id="item"/>
+      </div>
+      <button className="btn">Add</button>
+    </form>
+    <h1 className="header">TO DO LIST</h1>
+    <ul className="list">
+      <li>
+        <label>
+          <input type="checkbox"/>Item 1
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+    </ul>
+  </React.Fragment>
+  )
+}
