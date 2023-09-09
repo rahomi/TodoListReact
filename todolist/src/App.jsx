d0eca509a794fe019e8c489d63cd66103283ee78
rahomi <mohib.hossain.bu@gmail.com>
2023-09-09 21:33:52 +0600

    Use states implemented

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,24 +1,54 @@
 import React from "react"
+import { useState } from "react"
 import "./styles.css"
 
 export default function App(){
+  const [newItem,setNewItem] = useState("");
+  const [todos,setTodos] = useState([]);
+  function handleSubmit(e){
+    e.preventDefault();
+
+    setTodos(currentTodos =>{
+      return[
+        ...currentTodos,
+        {
+          id: crypto.randomUUID(),
+          title: newItem,
+          completed: false
+        },
+      ]
+    })
+
+    setNewItem("");
+  }
+
   return( 
   <React.Fragment>
-    <form className="new-item-form">
+    <form className="new-item-form" onSubmit={handleSubmit}>
       <div className="form-row">
             <label htmlFor="item">New Item</label>
-            <input type="text" id="item"/>
+            <input 
+            type="text" id="item"
+            value={newItem}
+            onChange={e => setNewItem(e.target.value)}
+            />
       </div>
       <button className="btn">Add</button>
     </form>
     <h1 className="header">TO DO LIST</h1>
     <ul className="list">
-      <li>
-        <label>
-          <input type="checkbox"/>Item 1
-        </label>
-        <button className="btn btn-danger">Delete</button>
-      </li>
+      {todos.map(todos => {
+        return(
+          <li key={todos.id}>
+          <label>
+            <input type="checkbox" checked={todos.completed}/>
+            {todos.title}
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        )
+      })}
+      
     </ul>
   </React.Fragment>
   )
